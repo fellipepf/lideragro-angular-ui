3f38b35519b7c18a8898a0eead8d0d84bfd13ac3
Fellipe <fellipepf@gmail.com>
2018-09-29 21:36:42 -0300

    validation messages and grid component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
@@ -8,6 +9,8 @@ import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { DropdownModule } from 'primeng/dropdown';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
 
 import { CurrencyMaskModule } from "ng2-currency-mask";
 
@@ -15,24 +18,30 @@ import { AppComponent } from './app.component';
 import { ProdutosBuscaComponent } from './produtos-busca/produtos-busca.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProdutosCadastroComponent } from './produtos-cadastro/produtos-cadastro.component';
+import { ProdutosGridComponent } from './produtos-grid/produtos-grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProdutosBuscaComponent,
     NavbarComponent,
-    ProdutosCadastroComponent
+    ProdutosCadastroComponent,
+    ProdutosGridComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+
     InputTextModule,
     ButtonModule,
     TableModule,
     TooltipModule,
     InputTextareaModule,
     DropdownModule,
-    CurrencyMaskModule
+    CurrencyMaskModule,
+    MessagesModule,
+    MessageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/produtos-grid/produtos-grid.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-produtos-grid',
+  templateUrl: './produtos-grid.component.html',
+  styleUrls: ['./produtos-grid.component.css']
+})
+export class ProdutosGridComponent  {
+  
+  @Input()  produtos = [];
+
+
+}

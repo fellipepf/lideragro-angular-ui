890816fb8e23b413505f3fc922663cf6b196199a
Fellipe <fellipepf@gmail.com>
2018-10-02 21:20:36 -0300

    refactoring modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,45 +3,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
-import { TableModule } from 'primeng/table';
-import { TooltipModule } from 'primeng/tooltip';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { DropdownModule } from 'primeng/dropdown';
-import { MessagesModule } from 'primeng/messages';
-import { MessageModule } from 'primeng/message';
-
-import { CurrencyMaskModule } from "ng2-currency-mask";
 
 import { AppComponent } from './app.component';
-import { ProdutosBuscaComponent } from './produtos-busca/produtos-busca.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ProdutosCadastroComponent } from './produtos-cadastro/produtos-cadastro.component';
-import { ProdutosGridComponent } from './produtos-grid/produtos-grid.component';
+import { ProdutosModule } from './produtos/produtos.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProdutosBuscaComponent,
-    NavbarComponent,
-    ProdutosCadastroComponent,
-    ProdutosGridComponent
+    AppComponent
+
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
 
-    InputTextModule,
-    ButtonModule,
-    TableModule,
-    TooltipModule,
-    InputTextareaModule,
-    DropdownModule,
-    CurrencyMaskModule,
-    MessagesModule,
-    MessageModule
+    ProdutosModule,
+    CoreModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/produtos/produtos.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { DropdownModule } from 'primeng/dropdown';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+
+import { ProdutosBuscaComponent } from './produtos-busca/produtos-busca.component';
+import { ProdutosCadastroComponent } from './produtos-cadastro/produtos-cadastro.component';
+import { ProdutosGridComponent } from './produtos-grid/produtos-grid.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+
+    CurrencyMaskModule,
+    
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule,
+    InputTextareaModule,
+    DropdownModule,
+    MessagesModule,
+    MessageModule
+  ],
+  declarations: [
+    ProdutosBuscaComponent,
+    ProdutosCadastroComponent,
+    ProdutosGridComponent
+  ],
+  exports: [
+    ProdutosBuscaComponent,
+    ProdutosCadastroComponent
+  ]
+})
+export class ProdutosModule { }

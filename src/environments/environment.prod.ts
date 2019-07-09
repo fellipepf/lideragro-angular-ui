ee82f614974967be0140b3e4be37ab3a20daf3e7
fellipe <fellipepf@gmail.com>
2019-07-10 00:03:23 +0100

    more changes  to production 3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
 
-  apiURL:  'http://localhost:8080',
+  apiURL:  'https://lideragro-api.herokuapp.com',
 
-  tokenWhitelistedDomains: [ new RegExp('localhost:8080') ],
+  tokenWhitelistedDomains: [ new RegExp('lideragro-api.herokuapp.com') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 };
